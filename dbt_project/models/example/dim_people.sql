37ac8f6f8a40d8bc66c3127716828f7cf851f8ab
Gabriel Watson <gabrielwatson2011@live.com>
2025-05-27 13:05:43 +0200

    ensure no issues with casting from float to int

--- dbt_project/models/example/dim_people.sql
+++ dbt_project/models/example/dim_people.sql
@@ -1,7 +1,7 @@
 select
    name
-  ,height
-  ,mass
+  ,CASE WHEN TRY_CAST(height AS FLOAT) IS NOT NULL THEN height ELSE NULL END as height
+  ,CASE WHEN TRY_CAST(mass AS FLOAT) IS NOT NULL THEN mass ELSE NULL END as mass
   ,gender
   ,birth_year
 from {{ ref('swapi_people') }}
