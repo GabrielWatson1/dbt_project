b0395c0595624c7b0cd98cad7b3cc133a231a3cd
Gabriel Watson <gabrielwatson2011@live.com>
2025-05-27 12:48:21 +0200

    created fact_people_metrics.sql table

---
+++ dbt_project/models/example/fact_people_metrics.sql
@@ -0,0 +1,16 @@
+with base as (
+    select
+        gender,
+        cast(height as float) as height,
+        cast(mass as float) as mass
+    from {{ ref('dim_people') }}
+    where height is not null and mass is not null
+)
+
+select
+    gender,
+    count(*) as people_count,
+    avg(height) as avg_height,
+    avg(mass) as avg_mass
+from base
+group by gender
