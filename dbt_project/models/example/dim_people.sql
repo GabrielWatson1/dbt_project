d29330c00100723a454dfe225b0746e09446aa5f
Gabriel Watson <gabrielwatson2011@live.com>
2025-05-27 13:07:15 +0200

    changes made to data type changes

--- dbt_project/models/example/dim_people.sql
+++ dbt_project/models/example/dim_people.sql
@@ -1,7 +1,7 @@
 select
    name
-  ,CASE WHEN TRY_CAST(height AS FLOAT) IS NOT NULL THEN height ELSE NULL END as height
-  ,CASE WHEN TRY_CAST(mass AS FLOAT) IS NOT NULL THEN mass ELSE NULL END as mass
+  ,height
+  ,mass
   ,gender
   ,birth_year
 from {{ ref('swapi_people') }}
--- dbt_project/models/example/fact_people_metrics.sql
+++ dbt_project/models/example/fact_people_metrics.sql
@@ -1,16 +1,16 @@
 with base as (
     select
-        gender,
-        cast(height as float) as height,
-        cast(mass as float) as mass
+        gender
+        ,height
+        ,mass
     from {{ ref('dim_people') }}
     where height is not null and mass is not null
 )
 
 select
     gender,
-    count(*) as people_count,
-    avg(height) as avg_height,
-    avg(mass) as avg_mass
+    count(*) as people_count
+    ,avg(height) as avg_height
+    ,avg(mass) as avg_mass
 from base
 group by gender
